f8ece3acdf7e35734729c23363deb3b5bb45ac6c
Goran-Arsov <arsphy@yahoo.com>
2019-04-08 17:30:29 +0200

    create new post view, fix posts_controller, add message template to application.html.erb

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,12 +8,20 @@ class PostsController < ApplicationController
   end
 
   def new
+    @category = Category.all
+    @post = Post.new
   end
 
   def edit
   end
 
   def create
+    @post = Post.new(permitted_params)
+    if @post.save
+      redirect_to root_path, :notice => "Your post is saved"
+    else
+      render "new", :notice => "Your post WAS NOT saved"
+    end
   end
 
   def update
@@ -22,4 +30,9 @@ class PostsController < ApplicationController
   def destroy
   end
   
+  private 
+
+    def permitted_params
+      params.require(:post).permit(:title, :body, :category_id)
+    end
 end

ccecd7d2cb222260603add3bd4df612b085557b2
Goran-Arsov <arsphy@yahoo.com>
2019-04-22 15:42:55 +0200

    make category index view, and show all the posts associated to a category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,8 +1,11 @@
 class CategoriesController < ApplicationController
   def index
+    @categories = Category.all
   end
 
   def show
+    @category = Category.find(params[:id])
+    @category_posts = @category.posts
   end
 
   def new
